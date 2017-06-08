8acb399c0fa1fa15157bce5e6af22a1ac34d40dc
Matei Radu <matei.radu.92@gmail.com>
2017-06-08 15:00:08 +0200

    Added Bootstrap; implemented footer.

--- src/Footer.js
+++ src/Footer.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
+import './Footer.css';
 
 class Footer extends Component{
   render() {
+    /* Links */
+    const githubLink = "https://github.com/mateiradu";
+    const twitterLink = "https://twitter.com/matei_dev";
+    const facebookLink = "https://www.facebook.com/mateiradu92";
+    const linkedinLink = "https://www.linkedin.com/in/mateibogdanradu/";
+
+    /* License specific */
+    const repoLink = githubLink + "/personal-website";
+    const licenseLink = repoLink + "/blob/master/LICENSE";
+    var currentYear = new Date().getFullYear();
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Footer Section</h2>
+      <div className="footer-root">
+        <div className="container"> 
+          <div className="footer-links">
+            <ul className="list-inline">
+              <li className="list-inline-item">
+                <a href={githubLink}><b>GitHub</b></a>
+              </li>
+              <li className="list-inline-item">
+                <a href={twitterLink}><b>Twitter</b></a>
+              </li>
+              <li className="list-inline-item">
+                <a href={facebookLink}><b>Facebook</b></a>
+              </li>
+              <li className="list-inline-item">
+                <a href={linkedinLink}><b>LinkedIn</b></a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-license">
+            <p>
+              Content Copyright © 2016-{currentYear} Matei Bogdan Radu.<br></br>
+              Designed and built by <a href={githubLink}>Matei Bogdan Radu</a>, 
+              code licensed under <a href={licenseLink}>MIT</a>, source 
+              available on <a href={repoLink}>Github</a>.
+            </p>
+          </div>
         </div>
-        <p className="App-intro">
-          <h2>Quick links</h2>
-          Copyright
-        </p>
       </div>
     );
   }
